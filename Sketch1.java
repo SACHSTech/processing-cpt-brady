358290b55750baf5ae0b2a60a3b6dc902867bda8
Brady So <99352833+bradyso@users.noreply.github.com>
2022-05-30 16:25:54 +0000

    setting up more arrays

--- Sketch1.java
+++ Sketch1.java
@@ -6,20 +6,21 @@ public class Sketch1 extends PApplet {
   // global variables
 
   // Teams
-	String[] strTeams = {"Arsenal", "Aston Villa", "Bournemouth", "Brentford", "Brighton", "Chelsea", "Crystal Palace", "Everton", "Fulham", "Leeds United", "Leicester City", "Liverpool", "Manchester City", "Manchester United", "Newcastle United", "TBD", "Southampton", "Tottenham Hotspur", "West Ham United", "Wolverhampton Wanderers"};
+	String[] strTeams = {"Arsenal", "Aston Villa", "Bournemouth", "Brentford", "Brighton", "Chelsea", "Crystal Palace", "Everton", "Fulham", "Leeds United", "Leicester City", "Liverpool", "Manchester City", "Manchester United", "Newcastle United", "Nottingham Forest", "Southampton", "Tottenham Hotspur", "West Ham United", "Wolverhampton Wanderers"};
 
   // Players
-  String[][] strPlayers = new String[20][0];
+  String[]strPlayers = new String[19];
 
   // Prediction
-  String[][] strPrediction = new String[20][0];
+  String[] strTeamPrediction = new String[19];
+  String[] strPlayerPrediction = new String[19];
 
   // Team Analysis
-  String[][] strAnalysis = new String[20][0];
+  String[] strAnalysis = new String[19];
 
   // Stats
-  String[][] strTeamStats = new String[20][6];
-  String[][] strPlayerStats = new String[20][6];
+  String[][] strTeamStats = new String[19][3];
+  String[][] strPlayerStats = new String[19][4];
 
   String strPlayerSelected;
   int intPlayerCount = 0;
@@ -30,12 +31,13 @@ public class Sketch1 extends PApplet {
 
   // Font Variables
   PFont title;
-  PFont section;
+  PFont analysis;
   PFont teamSelection;
   PFont header;
 
   // Image Variables
   PImage imgLogo;
+  PImage imgTeams;
   
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -54,6 +56,7 @@ public class Sketch1 extends PApplet {
 
     imgLogo = loadImage("premier-league-logo.png");
     imgLogo.resize(imgLogo.width / 3, imgLogo.height / 3);
+    imgTeams = loadImage("Teamlogos.png");
     
     // changing text
     title = createFont("Dialog.Input.bold", 80);
@@ -92,7 +95,7 @@ public class Sketch1 extends PApplet {
   * Author: Brady So
   */
   public void teamScreen() {
-
+    
   }
   /*
   * Creating team overview of analysis, player and prediction 
@@ -121,45 +124,191 @@ public class Sketch1 extends PApplet {
   */
   public void player() {
     // Leicester City
-    strPlayers[0][0] = "James Maddison";
+    strPlayers[0] = "James Maddison";
     // Bournemouth
-    strPlayers[1][0] = "Dominic Solanke";
+    strPlayers[1] = "Dominic Solanke";
     // Brighton
-    strPlayers[2][0] = "Marc Cucurella";
+    strPlayers[2] = "Marc Cucurella";
     // Brentford
-    strPlayers[3][0] = "Christian Eriksen";
+    strPlayers[3] = "Christian Eriksen";
     // Arsenal
-    strPlayers[4][0] = "Bukayo Saka";
+    strPlayers[4] = "Bukayo Saka";
     // Aston Villa
-    strPlayers[5][0] = "Phillipe Coutinho";
+    strPlayers[5] = "Matthew Cash";
     // Liverpool
-    strPlayers[6][0] = "Mohamed Salah";
+    strPlayers[6] = "Mohamed Salah";
     // Fulham
-    strPlayers[7][0] = "Alexander Mitrovic";
+    strPlayers[7] = "Alexander Mitrovic";
     // Southampton
-    strPlayers[8][0] = "James Ward-Prowse";
+    strPlayers[8] = "James Ward-Prowse";
     // Crystal Palace
-    strPlayers[9][0] = "Wilfred Zaha";
+    strPlayers[9] = "Wilfred Zaha";
     // Everton
-    strPlayers[10][0] = "Jordan Pickford";
+    strPlayers[10] = "Jordan Pickford";
     // Leeds United
-    strPlayers[11][0] = "Raphinha";
+    strPlayers[11] = "Raphinha";
     // Chelsea
-    strPlayers[12][0] = "Mason Mount";
+    strPlayers[12] = "Mason Mount";
     // West Ham United
-    strPlayers[13][0] = "Declan Rice";
+    strPlayers[13] = "Declan Rice";
     // Wolverhampton Wanderers
-    strPlayers[14][0] = "Ruben Neves";
+    strPlayers[14] = "Ruben Neves";
     // Tottenham Hotspur
-    strPlayers[15][0] = "Heung Min Son";
-    // team tbd
-    strPlayers[16][0] = "TBD";
+    strPlayers[15] = "Heung Min Son";
+    // Nottingham Forest
+    strPlayers[16] = "Brennan Johnson";
     // Newcastle United
-    strPlayers[17][0] = "Kieran Trippier";
+    strPlayers[17] = "Bruno Guimaraes";
     // Manchester United
-    strPlayers[18][0] = "Cristiano Ronaldo";
+    strPlayers[18] = "Cristiano Ronaldo";
     // Manchester City
-    strPlayers[19][0] = "Kevin De Bruyne";
+    strPlayers[19] = "Kevin De Bruyne";
+
+    // Stats
+    // Maddison
+    strPlayerStats[0][0] = "Appearances: 35";
+    strPlayerStats[0][1] = "Goals: 12";
+    strPlayerStats[0][2] = "Assists: 8";
+    strPlayerStats[0][3] = "Big Chances Created: 11";
+    strPlayerStats[0][4] = "Goals per Match: 0.34";
+    // Solanke
+    strPlayerStats[1][0] = "Appearances: 46";
+    strPlayerStats[1][1] = "Goals: 29";
+    strPlayerStats[1][2] = "Assists: 7";
+    strPlayerStats[1][3] = "Shooting Accuracy: 37.2%";
+    strPlayerStats[1][4] = "Goals per Match: 0.64";
+    // Cucurella
+    strPlayerStats[2][0] = "Appearances: 35";
+    strPlayerStats[2][1] = "Goals: 1";
+    strPlayerStats[2][2] = "Assists: 1";
+    strPlayerStats[2][3] = "Clean Sheets: 8";
+    strPlayerStats[2][4] = "Tackle Success: 67%";
+    // Eriksen
+    strPlayerStats[3][0] = "Appearances: 11";
+    strPlayerStats[3][1] = "Goals: 1";
+    strPlayerStats[3][2] = "Assists: 4";
+    strPlayerStats[3][3] = "Big Chances Created: 5";
+    strPlayerStats[3][4] = "Goals per Match: 0.09";
+    // Saka
+    strPlayerStats[4][0] = "Appearances: 38";
+    strPlayerStats[4][1] = "Goals: 11";
+    strPlayerStats[4][2] = "Assists: 7";
+    strPlayerStats[4][3] = "Big Chances Created: 8";
+    strPlayerStats[4][4] = "Goals per Match: 0.29";
+    // Cash
+    strPlayerStats[5][0] = "Appearances: 38";
+    strPlayerStats[5][1] = "Goals: 4";
+    strPlayerStats[5][2] = "Assists: 3";
+    strPlayerStats[5][3] = "Clean Sheets: 10";
+    strPlayerStats[5][4] = "Tackle Success: 56%";
+    // Salah
+    strPlayerStats[6][0] = "Appearances: 35";
+    strPlayerStats[6][1] = "Goals: 23";
+    strPlayerStats[6][2] = "Assists: 13";
+    strPlayerStats[6][3] = "Big Chances Created: 18";
+    strPlayerStats[6][4] = "Goals per Match: 0.66";
+    // Mitrovic
+    strPlayerStats[7][0] = "Appearances: 44";
+    strPlayerStats[7][1] = "Goals: 43";
+    strPlayerStats[7][2] = "Assists: 7";
+    strPlayerStats[7][3] = "Shooting Accuracy: 46.5%";
+    strPlayerStats[7][4] = "Goals per Match: 1.01";
+    // Ward-Prowse
+    strPlayerStats[8][0] = "Appearances: 36";
+    strPlayerStats[8][1] = "Goals: 10";
+    strPlayerStats[8][2] = "Assists: 5";
+    strPlayerStats[8][3] = "Big Chances Created: 8";
+    strPlayerStats[8][4] = "Goals per Match: 0.28";
+    // Zaha
+    strPlayerStats[9][0] = "Appearances: 33";
+    strPlayerStats[9][1] = "Goals: 14";
+    strPlayerStats[9][2] = "Assists: 1";
+    strPlayerStats[9][3] = "Big Chances Created: 4";
+    strPlayerStats[9][4] = "Goals per Match: 0.42";
+    // Pickford
+    strPlayerStats[10][0] = "Appearances: 35";
+    strPlayerStats[10][1] = "Clean Sheets: 7";
+    strPlayerStats[10][2] = "Saves: 117";
+    strPlayerStats[10][3] = "Goals Conceded: 58";
+    strPlayerStats[10][4] = "Errors Leading to Goals: 1";
+    // Raphinha
+    strPlayerStats[11][0] = "Appearances: 35";
+    strPlayerStats[11][1] = "Goals: 11";
+    strPlayerStats[11][2] = "Assists: 3";
+    strPlayerStats[11][3] = "Big Chances Created: 10";
+    strPlayerStats[11][4] = "Goals per Match: 0.31";
+    // Mount
+    strPlayerStats[12][0] = "Appearances: 32";
+    strPlayerStats[12][1] = "Goals: 11";
+    strPlayerStats[12][2] = "Assists: 10";
+    strPlayerStats[12][3] = "Big Chances Created: 10";
+    strPlayerStats[12][4] = "Goals per Match: 0.34";
+    // Rice
+    strPlayerStats[13][0] = "Appearances: 36";
+    strPlayerStats[13][1] = "Goals: 1";
+    strPlayerStats[13][2] = "Assists: 4";
+    strPlayerStats[13][3] = "Big Chances Created: 2";
+    strPlayerStats[13][4] = "Tackle Success: 53%";
+    // Neves
+    strPlayerStats[14][0] = "Appearances: 33";
+    strPlayerStats[14][1] = "Goals: 4";
+    strPlayerStats[14][2] = "Assists: 2";
+    strPlayerStats[14][3] = "Big Chances Created: 3";
+    strPlayerStats[14][4] = "Tackle Success: 54%";
+    // Son
+    strPlayerStats[15][0] = "Appearances: 35";
+    strPlayerStats[15][1] = "Goals: 23";
+    strPlayerStats[15][2] = "Assists: 7";
+    strPlayerStats[15][3] = "Big Chances Created: 10";
+    strPlayerStats[15][4] = "Goals per Match: 0.66";
+    // Johnson
+    strPlayerStats[16][0] = "Appearances: 49";
+    strPlayerStats[16][1] = "Goals: 18";
+    strPlayerStats[16][2] = "Assists: 10";
+    strPlayerStats[16][3] = "Shooting Accuracy: 41.9%";
+    strPlayerStats[16][4] = "Shots off Target: 28";
+    // Guimaraes
+    strPlayerStats[17][0] = "Appearances: 17";
+    strPlayerStats[17][1] = "Goals: 5";
+    strPlayerStats[17][2] = "Assists: 1";
+    strPlayerStats[17][3] = "Big Chances Created: 3";
+    strPlayerStats[17][4] = "Tackle Success: 52%";
+    // Ronaldo
+    strPlayerStats[18][0] = "Appearances: 30";
+    strPlayerStats[18][1] = "Goals: 18";
+    strPlayerStats[18][2] = "Assists: 3";
+    strPlayerStats[18][3] = "Shooting Accuracy: 39%";
+    strPlayerStats[18][4] = "Goals per Match: 0.60";
+    // De Bruyne
+    strPlayerStats[19][0] = "Appearances: 30";
+    strPlayerStats[19][1] = "Goals: 15";
+    strPlayerStats[19][2] = "Assists: 8";
+    strPlayerStats[19][3] = "Big Chances Created: 16";
+    strPlayerStats[19][4] = "Goals per Match: 0.50";
+    
+    // Player Prediction
+    strPlayerPrediction[0] = "James Maddison should continue to be the main focal point of Leicester City's attack and is expected to have another solid season. Status: Solid pick";
+    strPlayerPrediction[1] = "Solanke's breakthrough season in the lower tier of English football should be taken noticed but has not shown his ability in the Premier League in past years. Status: Risky";
+    strPlayerPrediction[2] = "As a defender, Marc Cucurella is a excellent option for your backline as he plays for a solid team and is involved in attacking plays as well. Status: Definitely pick."
+      strPlayerPrediction[3] = "Christian Eriksen's return from cardiac arrest is nothing short than spectacular! But as of right now, he is out of contract at Brentford and there are doubts he will stay. Status: Uncertain";
+    strPlayerPrediction[4] = "20 year old academy graduate Bukayo Saka has been solid according to his numbers. It is expected that he continues to improve his scoring and assisting for Arsenal. Status: Solid choice";
+    strPlayerPrediction[5] = "Matthew Cash was one of the top right backs in the league last year. If Aston Villa continue to be a solid team, Cash will be one of the best defenders avalible for your fantasy squad. Status: Definitely pick";
+    strPlayerPrediction[6] = "Mohamed Salah is one of the best players in the world because of his consistency to score goals for Liverpool. He is staying 100% next season and should be your first pick for your squad! Status: Pick for sure!";
+    strPlayerPrediction[7] = "Alexander Mitrovic scored a record breaking 43 goals last season in the lower tier. But, he has been poor in the Premier League in past seasons and Fulham are known to getting demoted when entering the Premier League. Status: Risky";
+    strPlayerPrediction[8] = "James Ward-Prowse is one ofthe best freekick takers in the world! However, his team is very inconsistent which can be very frustrating if he is in your squad. Status: Average pick.";
+    strPlayerPrediction[9] = "Wilfred Zaha has been Palace's best winger for several years. He has the teammates and talent to reach similar numbers to last season. Status: Solid pick";
+    strPlayerPrediction[10] = "Jordon Pickford was a part of a horrendus Everton team last season finishing with only 7 clean sheets ranked 15th in the league. It is very difficult to predict if Pickford and his team will improve. Status: Stay away";
+    strPlayerPrediction[11] = "Raphinha had a solid season even though he almost got relegated. There is uncertainty if he will remain a Leeds player next year as lots of clubs are interested in his services. Status: Uncertain";
+    strPlayerPrediction[12] = "Mason Mount had a excellent season for one of the best teams in the world. It is likely that he will be as good if not better next year. Status: Definitely pick"
+    strPlayerPrediction[13] = "Declan Rice was a huge part of his team reaching 7th in the league. However, he is more of a defensive midfielder and may not produce good fantasy number. Status: Would avoid";
+    strPlayerPrediction[14] = "Similar to Rice, Neves is a defensive midfielder but will have better statistics. But, it is likely he is leaving the team this summer. Status: Uncertain";
+    strPlayerPrediction[15] = "Heung Min Son shown he is a remarkable player tying with Salah for the most goals last season. Tottenham is likely to improve next season and Son should be a top pick for your squad. Status: Pick for sure";
+    strPlayerPrediction[16] = "Brennan Johnson was a break out striker in the lower tier division last season. However, he is a high risk pick as it is his first time in the Premier League. Status: Very risky";
+    strPlayerPrediction[17] = "Late signing Bruno Guimaraes proved his worth for Newcastle in only 17 games. Guimaraes is likely to have better numbers next season as he'll have more appearances. Status: Solid pick";
+    strPlayerPrediction[18] = "Cristiano Ronaldo was one of the very few Man United players who performed last season. Even though he will be 38 soon, one of the GOAT's of the game will be good choice for your squad. Status: Definitely pick";
+    strPlayerPrediction[19] = "Midfielder Kevin De Bruyne once again had the strongest impact in Man City winning the league. The player of the season will definetly put up solid numbers next season. Status: Pick for sure!";
+      
+    
   }
   
 }
--- Sketch2.java
+++ Sketch2.java
@@ -1,14 +1,49 @@
 import processing.core.PApplet;
+import processing.core.PFont; // Installing fonts
+import processing.core.PImage; // Import Image Library
 
-public class Sketch2 extends PApplet {
-	
-	
+public class Sketch1 extends PApplet {
+  // global variables
+
+  // Teams
+	String[] strTeams = {"Arsenal", "Aston Villa", "Bournemouth", "Brentford", "Brighton", "Chelsea", "Crystal Palace", "Everton", "Fulham", "Leeds United", "Leicester City", "Liverpool", "Manchester City", "Manchester United", "Newcastle United", "Nottingham Forest", "Southampton", "Tottenham Hotspur", "West Ham United", "Wolverhampton Wanderers"};
+
+  // Players
+  String[]strPlayers = new String[19];
+
+  // Prediction
+  String[] strTeamPrediction = new String[19];
+  String[] strPlayerPrediction = new String[19];
+
+  // Team Analysis
+  String[] strAnalysis = new String[19];
+
+  // Stats
+  String[][] strTeamStats = new String[19][3];
+  String[][] strPlayerStats = new String[19][4];
+
+  String strPlayerSelected;
+  int intPlayerCount = 0;
+  boolean blnScreen;
+  boolean blnPlayer;
+  boolean blnAnalysis;
+  boolean blnPrediction;
+
+  // Font Variables
+  PFont title;
+  PFont analysis;
+  PFont teamSelection;
+  PFont header;
+
+  // Image Variables
+  PImage imgLogo;
+  
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(600, 600);
   }
 
   /** 
@@ -16,21 +51,180 @@ public class Sketch2 extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(0, 255, 51);
+
+    imgLogo = loadImage("premier-league-logo.png");
+    imgLogo.resize(imgLogo.width / 3, imgLogo.height / 3);
+    
+    // changing text
+    title = createFont("Dialog.Input.bold", 80);
+    //header = createFont("");
+    section = createFont("Dialog.Input.italic", 20);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    mainScreen();
   }
   
   // define other methods down here.
-}
+  public void mouseClicked(){
+    
+  }
+  /*
+  * Creating main screen
+  * Author: Brady So
+  */
+  public void mainScreen() {
+    textFont(title, 25);
+    fill(102, 0, 153);
+    strokeWeight(15);
+    text("The 2022/2023 PREMIER LEAGUE Fantasy", 20, 50);
+    text("Helper and Predictor!", 150, 80);
+    image(imgLogo, 120, 90);
+    textFont(section, 25);
+    text("ENTER", 250, 500);
+    
+  }
+  /*
+  * Creating teams menu
+  * Author: Brady So
+  */
+  public void teamScreen() {
+    
+  }
+  /*
+  * Creating team overview of analysis, player and prediction 
+  * Author: Brady So
+  */
+  public void teamOverview() {
+    
+  }
+  /*
+  * Creating team analysis section 
+  * Author: Brian Gao
+  */
+  public void teamAnalysis() {
+    // Man City
+    strTeamStats[0][0] = "Points: 93";
+    strTeamStats[0][1] = "Wins: 29";
+    strTeamStats[0][2] = "Draws: 6"
+    strTeamStats[0][3] = "Losses: 3";
+    // Liverpool
+    strTeamStats[1][0] = "Points: 92";
+    strTeamStats[1][1] = "Wins: 28";
+    strTeamStats[1][2] = "Draws: 8"
+    strTeamStats[1][3] = "Losses: 2";
+    // Chelsea
+    strTeamStats[2][0] = "Points: 74";
+    strTeamStats[2][1] = "Wins: 21";
+    strTeamStats[2][2] = "Draws: 11"
+    strTeamStats[2][3] = "Losses: 6";
+    // Tottenham Hotspur
+    strTeamStats[3][0] = "Points: 71";
+    strTeamStats[3][1] = "Wins: 22";
+    strTeamStats[3][2] = "Draws: 5"
+    strTeamStats[3][3] = "Losses: 11";
+    // Arsenal
+    strTeamStats[4][0] = "Points: 69";
+    strTeamStats[4][1] = "Wins: 22";
+    strTeamStats[4][2] = "Draws: 3"
+    strTeamStats[4][3] = "Losses: 11";
+    // Manchester United
+    strTeamStats[5][0] = "Points: 58";
+    strTeamStats[5][1] = "Wins: 16";
+    strTeamStats[5][2] = "Draws: 10"
+    strTeamStats[5][3] = "Losses: 12";
+    // Westham United
+    strTeamStats[6][0] = "Points: 56";
+    strTeamStats[6][1] = "Wins: 16";
+    strTeamStats[6][2] = "Draws: 8"
+    strTeamStats[6][3] = "Losses: 14";
+    // Leicester City
+    strTeamStats[7][0] = "Points: 52";
+    strTeamStats[7][1] = "Wins: 14";
+    strTeamStats[7][2] = "Draws: 10"
+    strTeamStats[7][3] = "Losses: 14";
+    // Brighton and Hove Albion
+    strTeamStats[8][0] = "Points: 51";
+    strTeamStats[8][1] = "Wins: 12";
+    strTeamStats[8][2] = "Draws: 15"
+    strTeamStats[8][3] = "Losses: 11";
+    // Wolverhampton Wanderers
+    strTeamStats[9][0] = "Points: 51";
+    strTeamStats[9][1] = "Wins: 15";
+    strTeamStats[9][2] = "Draws: 6"
+    strTeamStats[9][3] = "Losses: 17";
+    // Newcastle United
+    strTeamStats[10][0] = "Points: 49";
+    strTeamStats[10][1] = "Wins: 13";
+    strTeamStats[10][2] = "Draws: 10"
+    strTeamStats[11][3] = "Losses: 15";
+    // Crystal Palace
+    strTeamStats[11][0] = "Points: 48";
+    strTeamStats[11][1] = "Wins: 11";
+    strTeamStats[11][2] = "Draws: 15"
+    strTeamStats[11][3] = "Losses: 12";
+    // Brentford
+    strTeamStats[12][0] = "Points: 46";
+    strTeamStats[12][1] = "Wins: 13";
+    strTeamStats[12][2] = "Draws: 7"
+    strTeamStats[12][3] = "Losses: 18";
+    // Aston Villa
+    strTeamStats[13][0] = "Points: 45";
+    strTeamStats[13][1] = "Wins: 13";
+    strTeamStats[13][2] = "Draws: 6"
+    strTeamStats[13][3] = "Losses: 19";
+    // Southampton
+    strTeamStats[14][0] = "Points: 40";
+    strTeamStats[14][1] = "Wins: 9";
+    strTeamStats[14][2] = "Draws: 13"
+    strTeamStats[14][3] = "Losses: 16";
+    // Everton
+    strTeamStats[15][0] = "Points: 39";
+    strTeamStats[15][1] = "Wins: 11";
+    strTeamStats[15][2] = "Draws: 6"
+    strTeamStats[15][3] = "Losses: 21";
+    // Leeds United
+    strTeamStats[16][0] = "Points: 38";
+    strTeamStats[16][1] = "Wins: 9";
+    strTeamStats[16][2] = "Draws: 11"
+    strTeamStats[16][3] = "Losses: 18";
+    // Fulham
+    strTeamStats[17][0] = "Points: 90";
+    strTeamStats[17][1] = "Wins: 27";
+    strTeamStats[17][2] = "Draws: 9"
+    strTeamStats[17][3] = "Losses: 10";
+    // Bournemouth
+    strTeamStats[18][0] = "Points: 88";
+    strTeamStats[18][1] = "Wins: 25";
+    strTeamStats[18][2] = "Draws: 13"
+    strTeamStats[18][3] = "Losses: 8";
+    // Notmm Forest
+    strTeamStats[19][0] = "Points: 83";
+    strTeamStats[19][1] = "Wins: 23";
+    strTeamStats[19][2] = "Draws: 11"
+    strTeamStats[19][3] = "Losses: 12";
+
+    // Team Analysis
+    strAnalysis[0] = "Manchester City has had a fantastic season with many wins under their belt and only a few draws and losses. Their roster consists of many well-known and strong players and with Pep Guardiola at the helm commanding the players, they are looking as one of the most dominant Premier League team we've seen in years.";
+    strAnalysis[1] = "Liverpool has had a great season with numerous wins and the fewest losses of the entire league. Second only next to Man City, they are the most reliable team in the league due to their unmatched consistency in the past few years. Their star players will lead them to win as many games as they possibly can.";
+    strAnalysis[2] = "With a squad full of world class players that can easily snatch the trophy, their season was nothing short of exceptional. However, the internal conflict between their star striker Lukaku and manager Thomas Tuchel over their change of playstyle led to damaging outcomes for the whole squad.";
+    strAnalysis[3] = "Tottenham";
+  }
+  /*
+  * Creating team prediction section 
+  * Author: Brian Gao
+  */
+  public void teamPrediction() {
+    strTeamPrediction[0] = "Man City will continue to be a title contender next season with their world class players, amazing manager and recently signed superstar striker Erling Haaland. Predicted Finish: 1st or 2nd";
+    strTeamPrediction[1] = "Liverpool are out for revenge after finishing 2nd for the second time in 4 years. The core of world class players are likely to stay and once again fight for the league title. Predicted Finish: 2nd or 1st":
+    strTeamPrediction[2] = "Chelsea are a excellent team but aren't consistent enough like Liverpool and Man City. If their players start to perform consistently, they have a chance to compete with the top 2 teams. Predicted Finish: 3rd";
+    strTeamPrediction[3] = "Tottenham had a miracle season reaching 4th place after a horrid start to last season. With manager Antonio Conte and new players incoming, Tottenham will look to improve next season. Predicted Finish: 4th or 3rd if lucky";
+    strTeamPrediction[4] = "Arsenal are the youngest team in the league but they lack the experience against the top teams. With the players having to play Europa League on Thursdays and Premier League Sunday games. It is uncertain if they can achieve a higher standing. Predicted Finish: 6th";
+    strTeamPrediction[5] = "Manchester United are under a new manager in Erik Ten Hag and look to improve from their worst point total of 58. With a huge number of players leaving and coming this summer, it is highly unpredictable what their season could be. Predicted finish: 5th or 6th";
+    
+    
+  }
